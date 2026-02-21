c049fed1ba6fc863b153b065259e3640ad4f9c65
Don-Fortune Tangban <74582825+donfortune@users.noreply.github.com>
2026-02-21 21:29:32 +0100

    Update update_changes_remotely.sh

--- update_changes_remotely.sh
+++ update_changes_remotely.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# Update submodules
+
 git submodule update --remote
 
 
 git add .
 
-# Commit with a message
+
 git commit -m "Updated submodules"
 
-# Push to the main branch
+
 git push origin main
 
 echo "Submodules updated and changes pushed successfully."
